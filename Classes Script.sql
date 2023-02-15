8e5be523dfbf6f71d4307cc64c02d3938b84f1c1
Carlos Barros <56875807+carlosefrbarros@users.noreply.github.com>
2023-02-14 22:20:59 -0300

    Added Content from 6th Class

--- Classes Script.sql
+++ Classes Script.sql
@@ -42,3 +42,54 @@ values
 
 select * from pessoas;
 
+#aula 6
+
+#descreve as colunas e as especificações
+describe pessoas;
+desc pessoas;
+
+alter table pessoas
+add column profissao varchar(10);
+
+alter table pessoas
+drop column profissao;
+
+alter table pessoas
+add column profissao varchar(10) after nome;
+
+alter table pessoas
+add codigo int first;
+
+alter table pessoas
+modify column profissao varchar(20);
+
+alter table pessoas
+modify profissao varchar(20) not null default '';
+
+alter table pessoas
+change column profissao prof varchar(20);
+
+alter table pessoas
+rename to gafanhotos;
+
+create table if not exists cursos (
+nome varchar(30) not null unique,
+descricao text,
+carga int unsigned,
+totalaulas int unsigned,
+ano year default '2016'
+) default charset utf8mb4;
+
+alter table cursos
+add column idcurso int first;
+
+describe cursos;
+
+alter table cursos
+drop primary key;
+
+alter table cursos
+add primary key idcurso;
+
+drop table cursos;
+
