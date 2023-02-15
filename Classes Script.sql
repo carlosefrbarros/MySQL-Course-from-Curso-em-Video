a66d87eabbbfc0550de22fc37c86cb1292ed763b
Carlos Barros <56875807+carlosefrbarros@users.noreply.github.com>
2023-02-15 15:52:01 -0300

    Added Content from 7th Class

--- Classes Script.sql
+++ Classes Script.sql
@@ -42,7 +42,7 @@ values
 
 select * from pessoas;
 
-#aula 6
+#6th class
 
 #descreve as colunas e as especificações
 describe pessoas;
@@ -96,3 +96,50 @@ modify column idcurso int primary key first;
 
 drop table cursos;
 
+#7th Class
+
+insert into cursos values
+('1', 'HTML4', 'Curso de HTML5', '40', '37', '2014'),
+('2', 'Algoritmos', 'Lógica de Progamação', '20', '15', '2014'),
+('3', 'Photoshop', 'Dicas de Photoshop CC', '10', '8', '2014'),
+('4', 'PGP', 'Curso de PHP para iniciantes', '40', '20', '2010'),
+('5', 'Jarva', 'Introdução à Linguagem Java', '10', '29', '2000'),
+('6', 'MySQL', 'Banco de Dados MySQL', '30', '15', '2016'),
+('7', 'Word', 'Curso completo de Word', '40', '30', '2016'),
+('8', 'Sapateado', 'Danças Rítmicas', '40', '37', '2018'),
+('9', 'Cozinha Árabe', 'Aprender a fazer Kibe', '40', '30', '2018'),
+('10', 'YouTuber', 'Gerar polêmica e ganhar inscritos', '5', '2', '2018');
+
+select * from cursos;
+
+update cursos
+set nome = 'HTML5'
+where idcurso = '1';
+
+select * from cursos;
+
+update cursos
+set nome = 'PHP', ano = '2015'
+where idcurso = '4';
+
+select * from cursos;
+
+update cursos
+set nome = 'Java', carga = '40', ano = '2015'
+where idcurso = '5'
+limit 1;
+
+select * from cursos;
+
+delete from cursos
+where idcurso = '8';
+
+select * from cursos;
+
+delete from cursos
+where ano = '2018'
+limit 1;
+
+truncate table cursos;
+
+select * from cursos;
