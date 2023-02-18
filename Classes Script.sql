f078a711366446842aef94accebc5c973f986aee
Carlos Barros <56875807+carlosefrbarros@users.noreply.github.com>
2023-02-18 18:37:29 -0300

    Added Content from 11th Class

--- Classes Script.sql
+++ Classes Script.sql
@@ -178,3 +178,60 @@ show tables;
 
 describe cursos;
 
+#11th Class
+
+use cadastro;
+
+select * from gafanhotos;
+
+select * from cursos 
+order by nome;
+
+select * from cursos
+order by nome desc;
+
+select * from cursos
+order by nome asc;
+
+select nome, carga, ano from cursos
+order by nome;
+
+select ano, nome, carga from cursos
+order by ano;
+
+select ano, nome, carga from cursos
+order by ano, nome;
+
+select * from cursos
+where ano = '2016'
+order by nome;
+
+select nome, carga from cursos
+where ano = '2016'
+order by nome;
+
+select nome, descricao, ano from cursos
+where ano <= '2015'
+order by ano, nome;
+
+select nome, descricao, ano from cursos
+where ano != '2016'
+order by ano, nome;
+
+select nome, descricao, ano from cursos
+where ano <> '2016'
+order by ano, nome;
+
+select nome, ano from cursos
+where ano between '2014' and '2016'
+order by ano desc, nome;
+
+select nome, descricao, ano from cursos
+where ano in ('2014', '2016')
+order by ano, nome;
+
+select nome, carga, totaulas from cursos
+where carga > 35 and totaulas < 30;
+
+
+
