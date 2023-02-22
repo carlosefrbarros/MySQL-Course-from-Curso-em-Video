487902271b29ecb435cff97e8bd1e1ac81e69324
Carlos Barros <56875807+carlosefrbarros@users.noreply.github.com>
2023-02-22 15:16:22 -0300

    Added Content from 15th Class

--- Classes Script.sql
+++ Classes Script.sql
@@ -404,3 +404,65 @@ where peso > '100'
 group by altura
 having altura > (select avg(altura) from gafanhotos)
 order by altura;
+
+#15th Class
+
+use cadastro;
+
+desc gafanhotos;
+
+alter table gafanhotos
+add column cursopreferido int;
+
+desc gafanhotos;
+
+alter table gafanhotos
+add foreign key (cursopreferido)
+references cursos (idcurso);
+
+desc gafanhotos;
+
+select * from gafanhotos;
+select * from cursos;
+
+update gafanhotos set cursopreferido = '6' where id = '1';
+
+select * from gafanhotos;
+
+delete from cursos
+where idcurso = '6';
+
+select nome, cursopreferido from gafanhotos;
+
+select nome, ano from cursos;
+
+select gafanhotos.nome, cursos.nome, cursos.ano 
+from gafanhotos join cursos
+on cursos.idcurso = gafanhotos.cursopreferido;
+
+select gafanhotos.nome, cursos.nome, cursos.ano 
+from gafanhotos join cursos
+on cursos.idcurso = gafanhotos.cursopreferido
+order by gafanhotos.id;
+
+select g.nome, c.nome, c.ano
+from gafanhotos as g inner join cursos as c
+on c.idcurso = g.cursopreferido
+order by g.nome;
+
+select g.nome, c.nome, c.ano
+from gafanhotos as g left outer join cursos as c
+on c.idcurso = g.cursopreferido;
+
+select g.nome, c.nome, c.ano
+from gafanhotos as g left join cursos as c
+on c.idcurso = g.cursopreferido;
+
+select g.nome, c.nome, c.ano
+from gafanhotos as g right outer join cursos as c
+on c.idcurso = g.cursopreferido;
+
+select g.nome, c.nome, c.ano
+from gafanhotos as g right join cursos as c
+on c.idcurso = g.cursopreferido;
+
